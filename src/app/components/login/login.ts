0379d8c62b25afa9367478e567356f1445d2292f
lucsaldias <luc.saldias@duocuc.cl>
2026-05-17 22:40:52 -0400

    Se añadio una configuracion que se asegura de que el correo cumpla con el dominio correspondiente (ya sea gmail, outlook, hotmail, entre otros y asegurandose que respete el formato con el '@')

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,39 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+const DOMINIOS_PERMITIDOS = ['gmail.com','hotmail.com','outlook.com'];
+
+export function validadorDominioCorreo(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const emailUser = control.value;
+
+    if (!emailUser) return null; //Si el espacio del campo de "correo electronico" esta vacio, el "validators.required" hara el trabajo de avisar que falta el correo.
+
+    const arrobaIndex = emailUser.lastIndexOf('@');
+
+    if (arrobaIndex === -1 || arrobaIndex === emailUser.length - 1) {
+      return null;
+    }
+
+    const dominio = emailUser.substring(arrobaIndex + 1).trim().toLowerCase();
+
+    const coincidenciaParcial = DOMINIOS_PERMITIDOS.some(d => d.startsWith(dominio));
+
+    const esValido = DOMINIOS_PERMITIDOS.includes(dominio);
+
+    if (esValido) {
+      return null;
+    }
+
+    if (!coincidenciaParcial) {
+      return { dominioInvalido: true };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-login',
@@ -15,7 +48,7 @@ export class Login {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      emailUser: ['', [Validators.required, Validators.email, validadorDominioCorreo()]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
@@ -25,5 +58,10 @@ export class Login {
       console.log(this.loginForm.value);
       this.router.navigate(['/dashboard']);
     }
+  }
+
+  ingresoInvitado(): void {
+    console.log('Ingresando como invitado');
+    this.router.navigate(['/dashboard']);
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { minLength } from '@angular/forms/signals';
 import { Router, RouterLink } from '@angular/router';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+const DOMINIOS_PERMITIDOS = ['gmail.com','hotmail.com','outlook.com'];
+
+export function validadorDominioCorreo(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const emailUser = control.value;
+
+    if (!emailUser) return null; //Si el espacio del campo de "correo electronico" esta vacio, el "validators.required" hara el trabajo de avisar que falta el correo.
+
+    const arrobaIndex = emailUser.lastIndexOf('@');
+
+    if (arrobaIndex === -1 || arrobaIndex === emailUser.length - 1) {
+      return null;
+    }
+
+    const dominio = emailUser.substring(arrobaIndex + 1).trim().toLowerCase();
+
+    const coincidenciaParcial = DOMINIOS_PERMITIDOS.some(d => d.startsWith(dominio));
+
+    const esValido = DOMINIOS_PERMITIDOS.includes(dominio);
+
+    if (esValido) {
+      return null;
+    }
+
+    if (!coincidenciaParcial) {
+      return { dominioInvalido: true };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-register',
@@ -19,7 +52,7 @@ export class Register implements OnInit {
   ngOnInit(): void {
     this.registerForm = this.fb.group({
       nameUser: ['', [Validators.required, Validators.minLength(3)]], //fsajfakja
-      emailUser: ['', [Validators.required, Validators.email]], 
+      emailUser: ['', [Validators.required, Validators.email, validadorDominioCorreo()]], 
       passwordUser: ['', [Validators.required, Validators.minLength(6)]],
       phoneUser: ['', [Validators.required, Validators.maxLength(9),Validators.minLength(9)]]
     });
