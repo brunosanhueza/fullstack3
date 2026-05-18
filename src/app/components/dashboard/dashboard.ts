ffd8e378a851d800bea1b8164204e5c232605fbd
lucsaldias <luc.saldias@duocuc.cl>
2026-05-17 23:13:22 -0400

    se agrego un footer que permite visualizar quienes somos, nuestra marca y otras opciones.

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../footer-component/footer-component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css'],
 })
