953502f4e9e1dfc72e3d337969b93f503fb42fd3
brunosanhueza <bruno.soto.04@outlook.cl>
2026-06-14 02:44:18 -0400

    cambios minimos, dependency swagger y redireccion de ip bd hacia la bd de produccion

--- src/main/java/com/loquendodev/carhub/controllers/usuarioController.java
+++ src/main/java/com/loquendodev/carhub/controllers/usuarioController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")  
+@CrossOrigin(origins = "http://3.209.17.222")  
 
 
 public class usuarioController {
